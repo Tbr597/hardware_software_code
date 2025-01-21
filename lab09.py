1b6895b865c6d36be06d889b2b7844f52acacca6
Tbr597 <118340647+Tbr597@users.noreply.github.com>
2025-01-21 10:57:16 -0500

    Update lab09.py

--- lab09.py
+++ lab09.py
@@ -1,9 +1,10 @@
 def print_lyrics():
-    print("I'm a programmer, and im I'm okay.")
-    print("I code all night, and  I code all day.")
+    print("I'm a programmer, and I'm okay.")
+    print("I code all night, and I code all day.")
 
 def repeat_lyrics(count = 1):
     for number in range(count):
+        print("{}#########".format(number),end=".")
         print_lyrics()
     print("#########")
 
