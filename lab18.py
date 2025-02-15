dadaf8968fba94a0135bfaee0655ccc5bcd75c55
Tbr597 <118340647+Tbr597@users.noreply.github.com>
2025-02-15 15:27:35 -0500

    Add files via upload

---
+++ lab18.py
@@ -0,0 +1,22 @@
+def display_menu(choose="Enter your choice"):
+  menu_dict = {"1":"apples",
+               "2":"bananas",
+               "3":"cherries",
+               "4":"grapes",
+}
+  print("Pick your favorite fruit")
+  for items, values in menu_dict.items():
+    print("\t{}. {}".format(items,values))
+  choice = input("{} ".format(choose)).upper().strip()
+  choices = list(menu_dict.keys())
+  if choice in choices:
+    return menu_dict[choice]
+  else:
+    return(display_menu("Invalid Selection\nTry Again:"))
+
+def main():
+  fruit = display_menu()
+  print("Wow, I like {} too!".format(fruit))
+        
+if __name__ == "__main__":
+    main()
